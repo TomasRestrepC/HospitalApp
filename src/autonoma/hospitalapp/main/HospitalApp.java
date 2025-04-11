41f97724388ed849cdcc96b1be1820af8054a189
Santiago Calderón <santiago.calderon.murillo@gmail.com>
2025-04-11 23:03:56 +0200

    Subiendo versión con capa de presentación

--- src/autonoma/hospitalapp/main/HospitalApp.java
+++ src/autonoma/hospitalapp/main/HospitalApp.java
@@ -2,24 +2,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package autonoma.hospitalapp;
-import javax.swing.SwingUtilities;
+package autonoma.hospitalapp.main;
+
 import autonoma.hospitalapp.views.VentanaPrincipal;
 
 /**
- * Representa la clase Main donde correran las ventanas 
- * de la capa de presentación
- * @author Santiago calderon murillo
- * Versión 1.0 
- * since@ 2025-04-08
+ * Clase principal que lanza la ventana de la aplicación.
+ * @author Santiago Calderón
+ * @version 1.0
+ * @since 2025-04-08
  */
-
 public class HospitalApp {
 
     public static void main(String[] args) {
-      SwingUtilities.invokeLater(() -> {
+        // Lanza la interfaz gráfica en el hilo de eventos
+        java.awt.EventQueue.invokeLater(() -> {
             new VentanaPrincipal().setVisible(true);
         });
     }
-    
 }
+
